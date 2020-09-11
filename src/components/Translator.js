402b11781cfcf9c5c3304fea730bcf3c4030069d
susarlanav <navyasusarla@gmail.com>
2020-09-11 16:21:40 -0500

    commit2

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -2,17 +2,39 @@ import React from 'react';
 
 class Translator extends React.Component {
 
+constructor(){
+  super();
+
+  this.state={
+    val:""
+  }
+}
+
+handleChange=(e)=>{
+  this.setState({
+      val : e.target.value
+  })
+}
+
   render() {
+
+      const { translations} = this.props;
     return (
       <React.Fragment>
         <div className="controls">
           <div className="input-container">
             <span>input:</span>
-            <input type="text" className="text-input" data-testid="text-input" />
+            <input type="text"
+            value={this.state.value}
+            className="text-input"
+            data-testid="text-input"
+            onChange={this.handleChange} />
           </div>
           <div className="input-container">
             <span>output:</span>
-            <input type="text" className="text-output" data-testid="text-output" readOnly />
+            <input
+            value ={translations.get(this.state.val)|| ''}
+            type="text" className="text-output" data-testid="text-output" readOnly />
           </div>
         </div>
       </React.Fragment>
